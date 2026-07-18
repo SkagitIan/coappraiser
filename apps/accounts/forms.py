02e7d6a96f8615c2b1aee6c1cf6eee1b07706cb0
SkagitIan <ian.larsen.1976@gmail.com>
2026-07-18 16:31:15 -0700

    Polish signup password form

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from django.contrib.auth.models import User
+
+
 class SignupForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"})
+    )
+
     class Meta:
         model = User
         fields = ["username", "email", "password"]
-    def save(self, commit=True):
-        user = super().save(commit=False); user.set_password(self.cleaned_data["password"])
-        if commit: user.save()
-        return user
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
